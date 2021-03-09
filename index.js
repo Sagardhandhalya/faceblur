0572c5a6aafbe2b85ba25f7c5738b3a491076dc3
Dhandhalya Sagar <dhandhalyasagar@FVFXTYGDJ1WK.local>
2021-03-09 17:16:19 +0530

    5*5 blur added..

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ window.addEventListener("load", () => {
     );
   }
   
-  video.addEventListener('play' ,drawOnCanvas )
+  video.addEventListener('play' , drawOnCanvas )
 
   function drawOnCanvas() {
     blur();
@@ -27,12 +27,11 @@ window.addEventListener("load", () => {
     }, 0);
   }
  
-  // for(let i = ((y-1)*wi+x)*4; i<((y+h)*wi+x+w)*4; i++)
-
   function blurLogic(pixelArray) {
     let pd = pixelArray.data;
     let wi = pixelArray.width;
-    for (let i = 4; i < pixelArray.data.length - 4; i++) {
+   
+    for (let i =parseInt((y-1)*4*wi + x*4) ; i < parseInt((y+h)*4*wi); i++) {
       let layerNo = i % 4;
       let pixelNo = i / 4;
       let a = pixelNo / wi;
@@ -40,45 +39,18 @@ window.addEventListener("load", () => {
 
       if (b > x && b < x + w && a > y && a < y + h) {
         if (layerNo === 3) continue;
-
-// 1 1 1 1 1
-// 1 1 1 1 1
-// 1 1 1 1 1
-// 1 1 1 1 1
-// 1 1 1 1 1
-
-        
         let temp=0;
-        
-        for(let j=-2;j<=2;j++)
+        for(let j=-3;j<=3;j++)
         {
-            for(let k=-2;k<=2;k++)
+            for(let k=-3;k<=3;k++)
             {
             temp += pd[i + 4*wi*j+4*k] || pd[i]
         }
-
-    
     }
-
- 
-       
-        pd[i] = temp/25;
-          // (pd[i] +
-          //   (pd[i - 4] || pd[i]) +
-          //   (pd[i - 8] || pd[i]) +
-          //   (pd[i + 4] || pd[i]) +
-          //   (pd[i + 8] || pd[i]) +
-          //   (pd[i - 4 * wi] || pd[i]) +
-          //   (pd[i - 8 * wi] || pd[i]) +
-          //   (pd[i + 4 * wi] || pd[i]) +
-          //   (pd[i + 8 * wi] || pd[i]) +
-          //   (pd[i - 4 * wi - 4] || pd[i]) +
-          //   (pd[i - 4 * wi + 4] || pd[i]) +
-          //   (pd[i + 4 * wi - 4] || pd[i]) +
-          //   (pd[i + 4 * wi + 4] || pd[i])) /
-          // 9;
+        pd[i] = temp/49;
       }
     }
+
     pixelArray.data = pd;
     return pixelArray;
   }
@@ -88,27 +60,25 @@ window.addEventListener("load", () => {
       .estimateFaces(video, false)
       .then((predictions) => {
         const prediction = predictions[0];
-        console.log(prediction);
+      
         const start = prediction.topLeft;
         const end = prediction.bottomRight;
         const size = [end[0] - start[0], end[1] - start[1]];
         let devideFactor = canvas.clientWidth /5;
-        console.log(canvas.clientWidth);
+    
         ctx.drawImage(video, 0, 0, 640, 480);
         // ctx.fillRect(start[0], start[1], size[0], size[1]);
         x = start[0];
         y = start[1]-90;
         h = size[1]+100;
         w = size[0];
-        console.log(x, y, h, w);
-
         let pixelArray = ctx.getImageData(
           0,
           0,
           canvas.clientWidth,
           canvas.clientHeight
         );
-        for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < 2; i++) {
           pixelArray = blurLogic(pixelArray);
         }
         
